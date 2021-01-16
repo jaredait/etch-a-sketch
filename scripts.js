59b8a60c452cabc25b0d5a0c8ce08c4ebf91f1c1
jaredait <jaredampudia@gmail.com>
2021-01-16 00:59:42 -0500

    Set up a mouseover effect so that the grid divs change color when the mouse passes over it

--- scripts.js
+++ scripts.js
@@ -3,7 +3,17 @@ const container = document.getElementById('container');
 for(let i = 0; i < 16; i++){
     let tempChild = document.createElement('div');
     tempChild.className = "cell";
+    tempChild.id = i;
     container.appendChild(tempChild);
 }
 
+let cells = Array.from(document.getElementsByClassName('cell'));
+console.log(cells[0]);
+cells.forEach(cell => cell.addEventListener('mouseover', leaveTrace));
+
 // Functions
+function leaveTrace(e){
+    console.log(e.target);
+    const pixel = document.getElementById(e.target.id);
+    pixel.style.backgroundColor = 'green';
+}
