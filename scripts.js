ba2f6ef97df0d067efb2534c2d35abd2c782a8ba
jaredait <jaredampudia@gmail.com>
2021-01-16 20:17:17 -0500

    Add clear button to reset the grid

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
+// Add a square grid of div elements
 const container = document.getElementById('container');
-
 for(let i = 0; i < 16; i++){
     let tempChild = document.createElement('div');
     tempChild.className = "cell";
@@ -7,13 +7,21 @@ for(let i = 0; i < 16; i++){
     container.appendChild(tempChild);
 }
 
+// Add a event listener to every cell
 let cells = Array.from(document.getElementsByClassName('cell'));
-console.log(cells[0]);
 cells.forEach(cell => cell.addEventListener('mouseover', leaveTrace));
 
-// Functions
+
+
+// FUNCTIONS
 function leaveTrace(e){
     console.log(e.target);
     const pixel = document.getElementById(e.target.id);
     pixel.style.backgroundColor = 'green';
 }
+
+clear = document.getElementById('clear-button');
+clear.addEventListener('click', () => {
+    console.log('jdjdjdjd');
+    cells.forEach(cell => cell.style.backgroundColor = 'palevioletred');
+});
