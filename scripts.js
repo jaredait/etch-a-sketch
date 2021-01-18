c6da4d7b835763a9a0fe156e5193eff176166d3e
jaredait <jaredampudia@gmail.com>
2021-01-18 01:03:47 -0500

    Add functionality to resize button. Fix variables and functions bugs

--- scripts.js
+++ scripts.js
@@ -3,33 +3,27 @@ const DEF_SIZE = 10;
 const DEF_BASE_COLOR = 'lightgray';
 const DEF_HOVER_COLOR = 'gray';
 
-
-
-// Create grid
+// Create initial grid
+const container = document.getElementById('container');
 createGrid(DEF_SIZE);
-container.style.gridTemplateColumns = `repeat(${DEF_SIZE}, 1fr)`;
-container.style.gridTemplateRows = `repeat(${DEF_SIZE}, 1fr)`;
-
-
-// Add a event listener to every cell
-let cells = Array.from(document.getElementsByClassName('cell'));
-cells.forEach(cell => cell.addEventListener('mouseover', leaveTrace));
-
 
 
 // FUNCTIONS and BUTTONS
 
-// Build a grid of size x size 
+// Build a grid of n x n (where n is the size parameter)
 function createGrid(size){
-// Add div elements to the container
-const container = document.getElementById('container');
-for(let i = 0; i < DEF_SIZE * DEF_SIZE; i++){
-    let tempChild = document.createElement('div');
-    tempChild.className = "cell";
-    tempChild.style.backgroundColor = DEF_BASE_COLOR;
-    tempChild.id = i;
-    container.appendChild(tempChild);
-}
+    // Add div elements to the container
+    const container = document.getElementById('container');
+    for(let i = 0; i < size * size; i++){
+        let tempChild = document.createElement('div');
+        tempChild.className = "cell";
+        tempChild.style.backgroundColor = DEF_BASE_COLOR;
+        tempChild.id = i;
+        container.appendChild(tempChild);
+        container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+        setHoverEffect();
+    }
 }
 
 // Leave trace
@@ -39,17 +33,30 @@ function leaveTrace(e){
     pixel.style.backgroundColor = DEF_HOVER_COLOR;
 }
 
+function setHoverEffect(){
+    // Add a event listener to every cell
+    let cells = Array.from(document.getElementsByClassName('cell'));
+    cells.forEach(cell => cell.addEventListener('mouseover', leaveTrace));
+}
+
 // Clear button
 clear = document.getElementById('clear-button');
 clear.addEventListener('click', () => {
-    console.log('jdjdjdjd');
+    let cells = Array.from(document.getElementsByClassName('cell'));
     cells.forEach(cell => cell.style.backgroundColor = DEF_BASE_COLOR);
 });
 
 // Resize button
 let resize = document.getElementById('resize-button');
 resize.addEventListener('click', () =>{
-    const newSize = prompt("Cell per edge:");
-
-
+    const newSize = prompt("Cell per edge:") || DEF_SIZE;
+    if(newSize >= 0 && newSize <= 50){
+        const cells = Array.from(document.getElementsByClassName('cell'));
+        for(let i = cells.length - 1; i >= 0; i--){
+            container.removeChild(cells[i]);
+        }
+        console.log(container.childElementCount);
+        createGrid(newSize);
+    }
+    else alert('Invalid input. Max number = 50');
 });
